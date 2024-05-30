39c51ab5638ce02d9bc4b5447836a5a88a93e017
Ellie <eleanorholt97@gmail.com>
2024-05-30 16:53:28 +0100

    Draft file structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import "./App.scss";
+import Search from "search/Search";
+import CurrentWeather from "currentweather/CurrentWeather";
+import WeatherForecast from "weatherforecast/WeatherForecast";
+import Footer from "footer/Footer";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hi</h1>
+      <h1>Weather Checker</h1>
+      <Search />
+      <CurrentWeather />
+      <WeatherForecast />
+      <Footer />
     </div>
   );
 }
