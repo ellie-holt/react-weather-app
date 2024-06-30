15e50ccb3df62876a265449e83c4f58fa4815782
Ellie <eleanorholt97@gmail.com>
2024-06-30 17:08:11 +0100

    npm update and alter page layout

--- src/App.js
+++ src/App.js
@@ -7,9 +7,6 @@ import Footer from "./footer/Footer";
 function App() {
   return (
     <div className="App">
-      <div className="MainHeading">
-        <h1>Weather Checker</h1>
-      </div>
       <Search />
       <CurrentWeather />
       <WeatherForecast />
--- src/search/Search.js
+++ src/search/Search.js
@@ -7,30 +7,27 @@ import "./Search.scss";
 export default function Search() {
   return (
     <div className="Search">
+      <div className="MainHeading">
+        <h1>Weather Checker</h1>
+      </div>
       <div className="SearchBar">
         <input
           type="search"
           id="search-bar"
+          className="SearchInput"
           placeholder="Type city here..."
           autoFocus
           required
           aria-label="User location"
           aria-describedby="search-button"
         />
-        <button type="submit" id="search-button">
+        <button type="submit" id="search-button" className="SearchButton">
           <FontAwesomeIcon icon={faMagnifyingGlass} />
         </button>
       </div>
-      {/* <div className="ConversionButton">
-        <button type="button" id="conversion-button">
-          Metric
-        </button>
-      </div> */}
-      <div className="CurrentButton">
-        <button type="button" id="current-button">
-          Current
-        </button>
-      </div>
+      <button type="button" id="current-button" className="CurrentButton">
+        Current
+      </button>
     </div>
   );
 }
