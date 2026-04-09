088f17c6fff19c04e4a84d49d83284b364863786
Ellie Holt <eleanorholt97@gmail.com>
2026-04-09 10:15:07 +0100

    Add breakpoint pip for development

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/index.scss";
 import App from "./App";
+import { Theme } from "@radix-ui/themes";
+import BreakpointPip from "./components/dev/BreakpointPip";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Theme>
+      <App />
+      <BreakpointPip />
+    </Theme>
   </React.StrictMode>
 );
