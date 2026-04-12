527a355a68a3a16147630548be28c5031991e898
Ellie Holt <eleanorholt97@gmail.com>
2026-04-12 20:05:13 +0100

    Remove BreakpointPip from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./styles/index.scss";
 import App from "./App";
 import { Theme } from "@radix-ui/themes";
-import BreakpointPip from "./components/dev/BreakpointPip";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Theme>
       <App />
-      <BreakpointPip />
     </Theme>
   </React.StrictMode>
 );
